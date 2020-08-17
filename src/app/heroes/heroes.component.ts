0592fde6e81f7574c451d1f69810c238359bbc08
zwarag <harrys.kavan@protonmail.com>
2020-08-17 16:04:44 +0200

    4. Add Services
    commit a15d75eafcde34270f1e8c40f91c3e73149e31db
    Author: zwarag <harrys.kavan@protonmail.com>
    Date:   Mon Aug 17 16:03:36 2020 +0200
    
        Display angular events in the Messages Component
    
    commit 27fb20d5e3984704680e0d32de420ee04c413191
    Author: zwarag <harrys.kavan@protonmail.com>
    Date:   Mon Aug 17 14:37:22 2020 +0200
    
        Make heroes Observable with rxjs
    
    commit e5600c90adc4e49e334dd6437751a44ae354a9aa
    Author: zwarag <harrys.kavan@protonmail.com>
    Date:   Mon Aug 17 14:27:39 2020 +0200
    
        Create a Service Component for Hero data

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+
+import { HeroService } from '../hero.service'
+import { MessageService } from '../message.service'
 
 @Component({
   selector: 'app-heroes',
@@ -9,16 +13,25 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
 }
